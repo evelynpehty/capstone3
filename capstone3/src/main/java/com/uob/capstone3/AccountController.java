5c4cec03fb4382d4a11b26461e3df14f0cdf4e97
evelynpehty <evelyn.peh.2020@scis.smu.edu.sg>
2023-10-20 15:28:23 +0800

    nav bar

---
+++ capstone3/src/main/java/com/uob/capstone3/AccountController.java
@@ -0,0 +1,14 @@
+package com.uob.capstone3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+    
+    @GetMapping("/viewAccounts")
+    public String showViewAccounts(Model M){
+        return "viewAccounts";
+    }
+}
