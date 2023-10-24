cb95caf7be2a11417ce9ae4c8a7c456f7e827924
HonWek <honwek_lim@live.com.my>
2023-10-24 11:17:52 +0800

    add transaction form

--- capstone3/src/main/java/com/uob/capstone3/AccountController.java
+++ capstone3/src/main/java/com/uob/capstone3/AccountController.java
@@ -3,11 +3,10 @@ package com.uob.capstone3;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class AccountController {
-    
     @GetMapping("/viewAccounts")
     public String showViewAccounts(Model M){
         return "viewAccounts";
@@ -17,5 +16,10 @@ public class AccountController {
     public String showAccountDashboard() {
         return "accountdashboard";
     }
-    
+
+    @GetMapping(value="/transact/{type}")
+    public String transact(@PathVariable(value = "type", required = true) String type, Model model) {
+        model.addAttribute("type", type);
+        return "transact";
+    }
 }
