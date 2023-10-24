b538a516dec4d88b200182141f7612fb4af62065
evelynpehty <evelyn.peh.2020@scis.smu.edu.sg>
2023-10-24 15:32:38 +0800

    create account

--- capstone3/src/main/java/com/uob/capstone3/Repositories/AccountRepository.java
+++ capstone3/src/main/java/com/uob/capstone3/Repositories/AccountRepository.java
@@ -1,12 +1,19 @@
 package com.uob.capstone3.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.uob.capstone3.Entities.Account;
+import com.uob.capstone3.Entities.AccountType;
+
 import java.util.List;
 
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
     List<Account> findByCustomerNRIC(String customerNRIC);
-}
+    
+    @Query("SELECT a FROM Account a WHERE a.accountType = :accountType AND a.customerNRIC = :customerNRIC")
+    List<Account> findByAccountTypeAndCustomerNRIC(@Param("accountType") AccountType accountType, @Param("customerNRIC") String customerNRIC);
+    }
