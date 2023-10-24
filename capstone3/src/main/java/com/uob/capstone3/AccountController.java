30c06dcf5584a6d190643ad04e005de9152ff35e
Kacey Chua <110165967+kacey152@users.noreply.github.com>
2023-10-24 11:09:35 +0800

    added transaction history to dashboard

--- capstone3/src/main/java/com/uob/capstone3/AccountController.java
+++ capstone3/src/main/java/com/uob/capstone3/AccountController.java
@@ -1,12 +1,18 @@
 package com.uob.capstone3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.uob.capstone3.Entities.AccountTransaction;
+import com.uob.capstone3.Repositories.AccountTransactionRepository;
+
 
 @Controller
 public class AccountController {
+    @Autowired
+    AccountTransactionRepository atr;
     
     @GetMapping("/viewAccounts")
     public String showViewAccounts(Model M){
@@ -14,7 +20,8 @@ public class AccountController {
     }
 
     @GetMapping(value="/accountdashboard")
-    public String showAccountDashboard() {
+    public String showAccountDashboard(Model m) {
+        m.addAttribute("transactions", atr.findAll());
         return "accountdashboard";
     }
     
