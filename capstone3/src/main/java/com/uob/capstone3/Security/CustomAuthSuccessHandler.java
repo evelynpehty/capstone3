697a43f25b2f659b5c18e3ba4eb54b957646f64b
Kacey Chua <110165967+kacey152@users.noreply.github.com>
2023-10-26 10:31:07 +0800

    fixed double login bug

--- capstone3/src/main/java/com/uob/capstone3/Security/CustomAuthSuccessHandler.java
+++ capstone3/src/main/java/com/uob/capstone3/Security/CustomAuthSuccessHandler.java
@@ -1,21 +1,30 @@
 package com.uob.capstone3.Security;
 
+import java.io.IOException;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
-public class CustomAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+public class CustomAuthSuccessHandler implements AuthenticationSuccessHandler {
 
-    @Override
-    protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+     @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, 
+                                        Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(authentication);
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(Authentication authentication) {
         // Your logic to determine the target URL based on the user's roles
         if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("Admin"))) {
             return "/Admin/viewTellers";
