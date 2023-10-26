551d1abed256fdb675723a521c5e7d03b7cd7940
HonWek <honwek_lim@live.com.my>
2023-10-26 15:49:02 +0800

    format currency

--- capstone3/src/main/java/com/uob/capstone3/Security/CustomAuthSuccessHandler.java
+++ capstone3/src/main/java/com/uob/capstone3/Security/CustomAuthSuccessHandler.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import jakarta.servlet.http.HttpServletRequest;
